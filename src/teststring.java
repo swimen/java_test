45202f2b5846f0082454a489af07b337c70df6cf
Aleksey Golovin <Aleksey.Golovin@stash.billing.com>
2021-04-05 19:58:29 +0700

    test commit

--- src/teststring.java
+++ src/teststring.java
@@ -5,7 +5,7 @@ public class teststring {
         String s1 = "tstr1";
         String s2 = "tstr1";
         String s3 = new String("tstr1");
-
+        // ---- 
         System.out.println((s1==s2)? "true":"false");
         System.out.println((s1==s3)? "true":"false");
         System.out.println((s2==s3)? "true":"false");
